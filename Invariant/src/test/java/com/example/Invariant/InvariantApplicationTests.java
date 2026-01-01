34ebf431659768e2945a8598cdaff299211d8176
SujalPatil21 <sujalbpatil21@gmail.com>
2026-01-01 23:36:17 +0530

    Bootstrap invariant-core with ping endpoint

---
+++ Invariant/src/test/java/com/example/Invariant/InvariantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Invariant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvariantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
